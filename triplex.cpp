470560dd6510940dd59aa1ddd3fbe795fb273b5e
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-07-05 10:11:51 +0200

    Update triplex.cpp

--- triplex.cpp
+++ triplex.cpp
@@ -9,6 +9,20 @@ int main() {
 	cout << "You need to enter the correct codes to continue..." << endl;
 	cout << endl;
 	cout << endl;
+	
+	int CodeA = 3;
+	int CodeB = 2;
+	int CodeC = 1;
+
+	int CodeSum = CodeA + CodeB + CodeC;
+	int CodeProduct = CodeA * CodeB * CodeC;
+
+	cout << "+ There are 3 numbers in the code..." << endl;
+	cout << "+ The codes add-up to " << CodeSum << endl;
+	cout << "+ And the codes multiply to give " << CodeProduct << endl;
+	cout << endl;
+
+
 
 
 
