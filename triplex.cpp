6f5aba227090bcdd3bc9fe6017ce7acd2e3e169c
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-07-05 10:11:37 +0200

    Update triplex.cpp

--- triplex.cpp
+++ triplex.cpp
@@ -5,3 +5,10 @@ using namespace std;
 
 int main() {
 
+	cout << "You are a secret agent breaking into a secure server room." << endl;
+	cout << "You need to enter the correct codes to continue..." << endl;
+	cout << endl;
+	cout << endl;
+
+
+
