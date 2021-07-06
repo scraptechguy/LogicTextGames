bfd8f30a64bd77e802b64bc44dcca812e7c392ee
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-07-06 13:50:35 +0200

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -10,3 +10,14 @@ int Sol;
 int main() {
 
 	while (true) {
+		
+
+		cout << "\nEnter the first number: ";
+		cin >> Num1;
+
+		cout << "\nEnter the operator: ";
+		cin >> Op;
+
+		cout << "\nEnter the second number: ";
+		cin >> Num2;
+
