49d6fe938d49faef17af8983fe7736e921819ddb
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-07-08 20:47:29 +0200

    Update triplex.cpp

--- triplex.cpp
+++ triplex.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <ctime>
+#include <algorithm>
+#include <list>
+
+using namespace std;
+
+void PrintIntro(int Diff) {
+
+	cout << "\n\nYou are a secret agent breaking into a super secured data center." << endl;
+	cout << "You need to enter the correct codes to continue..." << endl;
+
+}
+
+bool bTopSecret = true;
+
+int CodeA = 1;
+int CodeB = 1;
+int CodeC = 1;
+
+int CodeSum = 1;
+int CodeProduct = 1;
+
 
